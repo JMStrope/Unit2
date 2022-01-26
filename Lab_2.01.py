73bd20e67fce866bbacb75a434b36b9c13a942eb
Jack <Jmstrope24@students.bmchs.org>
2022-01-26 10:09:38 -0600

    files added

---
+++ Lab_2.01.py
@@ -0,0 +1,23 @@
+'''
+In your notebook
+Predict what the following inputs will result in
+Once you have filled in the "prediction" column, check your answers in interactive mode and write the actual result.
+    Input                                       Prediction                                      Result
+1.  float('1')                                      1
+2.  str(1 + '2')                                    12
+3.  str('2')                                        2
+4.  int('abc')                                      abc
+5.  int(float('1.6'))                               1.6
+6.  float(int(1.6))                                 1.6
+7.  str(float(1))                                   1
+
+
+In script mode
+Create a program which will take in an input and print out that input divided by 2.
+Alter one line of that program to return only whole numbers.
+
+'''
+
+name = int(input("Please give a number that can be divided by 2 >"))
+print({name} * 2)
+
