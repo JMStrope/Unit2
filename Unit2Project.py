08eec7d315d9f2017110a7fca64d9f4fa8d8db45
Jack <Jmstrope24@students.bmchs.org>
2022-02-22 09:13:46 -0600

    files added

--- Unit2Project.py
+++ Unit2Project.py
@@ -1,4 +1,5 @@
 #Map of dungeon
+
 dungeon = [
     ['prize','boss monster', 'sword', 'stairs down', 'sword'],
     ['magic stones','monster','stairs down','empty','stairs up'],
@@ -10,10 +11,11 @@ inventory = []
 current_room = 0
 current_floor = 2
 location = dungeon[current_floor][current_room]
+last_movement = ''
 
 #game loop
-while True:
-
+game_over = False
+while game_over == False:
     #Update Location
     location = dungeon[current_floor][current_room]
 
@@ -32,10 +34,16 @@ while True:
         print("You find a room with magic stones")
     elif location == 'boss monster':
         print("You have encounter the Boss monster, Defeating him will allow you to escape")
-    
+    elif location == 'stairs down':
+        print("You see a stair case leading back down")
+
+    elif location == 'prize':
+        print("You have successfully cleared the dungeon. Your prize is $1,000,000,000")
+        break
     #player choices
     player_input = input("What would you like to do? (Left, Right, Up, Down, Grab, Fight, Inventory) > ")
     if player_input == 'Right' :
+        last_movement = 'Right'
         current_room += 1
         if current_room == 5:
             print("You cannot move any further that way")
@@ -63,18 +71,38 @@ while True:
         else:
             print("There is nothing to grab here")
     elif player_input == 'Inventory':
-        print("you have:")
+        print("you have:") 
         print(' '.join(inventory))
     elif player_input == 'Fight':
-        if location == 'monster' and 'sword' in inventory:
-            print("You defeated the Big Monster")
-            dungeon[current_floor][current_room] = 'empty'
-    elif player_input == 'Fight':
-        if location == 'boss monster' and ('sword' in inventory and 'magic stones' in inventory):
-            print("You defeated the Dungeons Boss Monster, Congratulations. You may now escape the Dungeon")
-            break
+        if location == 'monster':
+            if 'sword' in inventory:
+                print("You defeated the Big Monster")
+                print("But your sword was destroyed in the fight")
+                inventory.remove('sword')
+                dungeon[current_floor][current_room] = 'empty'
+            else: 
+                ("You tried to fight a big monster without a weapon.")
+                game_over = True 
+        elif location == 'boss monster':
+            if 'sword' in inventory and 'magic stones' in inventory:
+                print("You defeated the Dungeons Boss Monster, Congratulations. You may now collect your prize and escape the Dungeon")
+                dungeon[current_floor][current_room] = 'empty'
+            else:
+                print("You fought the Boss Monster without the necessary equipment. YOU DIED")
+                game_over = True
         else:
-            print("There are no enemies nearby you can fight right now")
+            print("There are no monsters to fight")
+    if location == 'monster' or 'boss monster' and player_input: pass
+
+    #Play again
+    if game_over == True:
+        player_input = input("Would you like to play again? (y/n)")
+        if player_input == 'y':
+            game_over = False
+        else:
+            break
+        
+    
 
             
 
